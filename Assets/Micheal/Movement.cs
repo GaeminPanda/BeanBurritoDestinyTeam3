2dae3e463c8d4de2266a8bbe1ebf9aae011515c6
MN <1709731@mukilteo.wednet.edu>
2021-11-23 09:58:34 -0800

    Added some movement mechanics on C# script for character

---
+++ Assets/Micheal/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float horizontal;
+    [SerializeField]
+    private float moveSpeed = 1f;
+    Rigidbody2D rb2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2.velocity = new Vector2(horizontal * moveSpeed, rb2.velocity.y);//Basic Movement
+    }
+
+}
