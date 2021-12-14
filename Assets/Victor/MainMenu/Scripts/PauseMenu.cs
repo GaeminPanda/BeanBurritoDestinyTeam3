c1e93a7e15631a1bc73b562bae0dfbeedd83b02a
MUKILTEO\1710204 <1710204@mukilteo.wednet.edu>
2021-12-14 10:16:52 -0800

    PauseMenu

--- Assets/Victor/MainMenu/Scripts/PauseMenu.cs
+++ Assets/Victor/MainMenu/Scripts/PauseMenu.cs
@@ -42,7 +42,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Main Menu");
     }
 
     public void QuitGame()
