7de1a9d61df20030e638b483b20336df38bf6bc1
MUKILTEO\1710692 <katarina.ramos03@icloud.com>
2022-01-10 08:52:42 -0800

    WorkingOnRespawn

---
+++ Assets/Katarina/Scripts/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
