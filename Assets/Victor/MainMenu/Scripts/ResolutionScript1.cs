7471b4fdcc808e2e85d1c32a70965c0f22aa0501
MUKILTEO\1710204 <1710204@mukilteo.wednet.edu>
2022-01-10 08:52:15 -0800

    Pause Button

---
+++ Assets/Victor/MainMenu/Scripts/ResolutionScript1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1024, 768, Screen.fullScreen);
+    }
+
+    
+}
