b1ee8a8b071584ec0a039fe75b057029fca41622
MUKILTEO\1710204 <1710204@mukilteo.wednet.edu>
2022-01-11 10:21:14 -0800

    GameOver, End Comic, scene changes

---
+++ Assets/Victor/GameOver/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void LoadNextThing()
+    {
+        SceneManager.LoadScene("CounterTopScene1");
+    }
+    public void LoadNextThing2()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
