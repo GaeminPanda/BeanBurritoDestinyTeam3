74ad26c14c64923eb5ea4a6c4885b92ad52febcd
MUKILTEO\1710204 <1710204@mukilteo.wednet.edu>
2022-01-12 10:21:18 -0800

    Pause Menu, Shooting stuff

---
+++ Assets/Victor/TestDannyAttack/Scripts/BeanBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeanBullet : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+
+    public GameObject destroyEffect;
+
+    private void Start()
+    {
+        Invoke("destroyprojectile", lifetime); 
+    }
+
+    private void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void Destroyprojectile()
+    {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
