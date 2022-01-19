4841ef9e2ced3ffb1768d80f7f9332636775bb60
MUKILTEO\1710692 <katarina.ramos03@icloud.com>
2022-01-19 09:21:03 -0800

    Added gameover

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "TestPlayer")
+        {
+            Debug.Log("Switch Scene");
+            SceneManager.LoadScene("EndComic");
+        }
+    }
+}
